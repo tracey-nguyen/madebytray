4459ac4e025960a4c81d5ce163dcfe8930122eb9
tracey-nguyen <tracey_nguyen_00@hotmail.com>
2024-04-24 22:00:24 +1000

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      Hello! This is a website dedicated to Tracey's crochet projects :)
+    </div>
+  );
+}
+
+export default App;
